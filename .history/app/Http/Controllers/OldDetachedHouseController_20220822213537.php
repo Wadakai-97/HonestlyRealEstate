a88f5bf23a73480acc586bd95d6f3e28bb7529b7
Wadakai-97 <wadakai1111@gmail.com>
2022-08-22 21:54:15 +0900

    CSV機能（トライ）

---
+++ .history/app/Http/Controllers/OldDetachedHouseController_20220822213537.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use App\Models\Recommend;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use App\Models\OldDetachedHouse;
+use App\Models\OldDetachedHouseImage;
+use App\Http\Requests\ImageSignUpRequest;
+use App\Http\Requests\DetachedHouseSearchRequest;
+use App\Http\Requests\OldDetachedHouseSignUpRequest;
+
+class OldDetachedHouseController extends Controller
+{
+    // For User
+    public function top() {
+        return view('user.old_detached_house.top');
+    }
+    public function search(DetachedHouseSearchRequest $request) {
+        $old_detached_houses = OldDetachedHouse::select('id', 'name', 'price', 'pref', 'municipalities', 'land_area', 'building_area', 'number_of_rooms', 'type_of_room', 'year', 'land_right', 'station', 'walking_distance_station', 'updated_at')
+                                                ->wherePlan($request)
+                                                ->wherePref($request)
+                                                ->whereMunicipalities($request)
+                                                ->whereLowestPrice($request)
+                                                ->whereHighestPrice($request)
+                                                ->whereLowestLandArea($request)
+                                                ->whereHighestLandArea($request)
+                                                ->whereLowestBuildingArea($request)
+                                                ->whereHighestBuildingArea($request)
+                                                ->whereOld($request)
+                                                ->whereStation($request)
+                                                ->whereWalkingDistanceStation($request)
+                                                ->whereLandRight($request)
+                                                ->paginate(15);
+
+        $today = Carbon::today()->year;
+        $request->session()->regenerateToken();
+        return view('user.old_detached_house.result', compact('old_detached_houses', 'request', 'today'));
+    }
+    public function detail() {
+        return view('user.old_detached_house.detail');
+    }
+    public function sort(Request $request) {
+        $old_detached_house = new OldDetachedHouse;
+        $column = $old_detached_house->column($request);
+        $type = $old_detached_house->sort($request);
+
+        $old_detached_houses = OldDetachedHouse::select('id', 'name', 'price', 'pref', 'municipalities', 'land_area', 'building_area', 'number_of_rooms', 'type_of_room', 'year', 'land_right', 'station', 'walking_distance_station', 'updated_at')
+                                                ->wherePlan($request)
+                                                ->wherePref($request)
+                                                ->whereMunicipalities($request)
+                                                ->whereLowestPrice($request)
+                                                ->whereHighestPrice($request)
+                                                ->whereLowestLandArea($request)
+                                                ->whereHighestLandArea($request)
+                                                ->whereLowestBuildingArea($request)
+                                                ->whereHighestBuildingArea($request)
+                                                ->whereOld($request)
+                                                ->whereStation($request)
+                                                ->whereWalkingDistanceStation($request)
+                                                ->whereLandRight($request)
+                                                ->orderBy($column, $type)
+                                                ->paginate(15);
+
+        $today = Carbon::today()->year;
+        return view('user.old_detached_house.result', compact('old_detached_houses', 'request', 'today'));
+    }
+
+    // For Admin
+    public function showForm() {
+        return view('admin.old_detached_house.sign_up');
+    }
+    public function signUp(OldDetachedHouseSignUpRequest $request) {
+        $post = new OldDetachedHouse;
+        $post->signUp($request);
+        $request->session()->regenerateToken();
+        return redirect('admin.oldDetachedHouse.sign_up')->with('message', '登録が完了しました。');
+    }
+
+    // Image
+    public function imageSignUp($id, ImageSignUpRequest $request) {
+        $old_detached_house_image = new OldDetachedHouseImage;
+        $old_detached_house_image->imageSignUp($id, $request);
+        $old_detached_house = OldDetachedHouse::find($id);
+        $old_detached_house_images = OldDetachedHouseImage::where('old_detached_house_id', '=', $id)->get();
+        $image_counter = 1;
+        $request->session()->regenerateToken();
+        return view('admin.old_detached_house.edit_image', compact('old_detached_house', 'image_counter', 'old_detached_house_images'))->with('message', '画像登録が完了しました。');
+    }
+    public function imageDelete($id) {
+        $old_detached_house_image = OldDetachedHouseImage::find($id);
+        $old_detached_house_id = $old_detached_house_image->old_detached_house_id;
+
+        DB::transaction(function() use($id) {
+            $old_detached_house_image = OldDetachedHouseImage::find($id);
+            Storage::delete('storage/property_images/old_detached_house/' . $old_detached_house_image->path);
+            $old_detached_house_image->delete();
+        });
+
+        $old_detached_house = OldDetachedHouse::find($old_detached_house_id);
+        $old_detached_house_images = OldDetachedHouseImage::where('old_detached_house_id', '=', $old_detached_house_id)->get();
+        $existing_images = OldDetachedHouseImage::where('old_detached_house_id', '=', $old_detached_house_id)->count();
+
+        if(!empty($existing_images)) {
+            $image_counter = $existing_images;
+        } else if(empty($existing_images)) {
+            $image_counter = 1;
+        }
+
+        return view('admin.old_detached_house.edit_image', compact('old_detached_house', 'old_detached_house_images', 'image_counter'));
+    }
+    public function imageUpdate($id, ImageSignUpRequest $request) {
+        $old_detached_house_image = OldDetachedHouseImage::find($id);
+        $old_detached_house_image->imageUpdate($id, $request);
+        $old_detached_house = OldDetachedHouse::find($old_detached_house_image->old_detached_house_id);
+        $old_detached_house_images = OldDetachedHouseImage::where('old_detached_house_id', '=', $id)->get();
+        $image_counter = 1;
+        $request->session()->regenerateToken();
+        return view('admin.old_detached_house.edit_image', compact('old_detached_house', 'old_detached_house_images','id', 'image_counter'));
+    }
+    public function imageEdit($id) {
+        $old_detached_house = OldDetachedHouse::find($id);
+        $old_detached_house_images = OldDetachedHouseImage::where('old_detached_house_id', '=', $id)->get();
+        $image_counter = 1;
+        return view('admin.old_detached_house.edit_image', compact('old_detached_house', 'old_detached_house_images', 'image_counter'));
+    }
+
+
+    public function list() {
+        $old_detached_houses = OldDetachedHouse::select('id', 'pref', 'municipalities', 'block', 'number_of_rooms', 'type_of_room', 'price', 'land_right', 'year', 'land_area', 'building_area', 'station', 'walking_distance_station')
+                                            ->paginate(15);
+
+        return view('admin.old_detached_house.list', compact('old_detached_houses'));
+    }
+
+    // Recommend
+    public function recommendSignUp($id) {
+        $old_detached_house = OldDetachedHouse::find($id);
+        $old_detached_house->recommend($id);
+        return redirect()->route('admin.oldDetachedHouse.list')->with('message', 'お気に入り登録が完了しました。');
+    }
+    public function recommendDelete($id) {
+        $recommend_old_detached_house = Recommend::where('old_detached_house_id', $id)->get();
+
+        if(!empty($recommend_old_detached_house)) {
+            Recommend::where('old_detached_house_id', $id)->delete();
+            $message = 'successMessage';
+            $flash_message = "おすすめ登録を削除しました。";
+        } else {
+            $message = 'errorMessage';
+            $flash_message = "削除に失敗しました。";
+        }
+
+        return redirect()->route('admin.recommend.list')->with($message, $flash_message);
+    }
+
+    public function delete($id) {
+        DB::transaction(function() use($id) {
+            $target = OldDetachedHouse::find($id);
+            $target->delete();
+        });
+    }
+    public function adminSearch(DetachedHouseSearchRequest $request) {
+        $old_detached_houses = OldDetachedHouse::select('id', 'pref', 'municipalities', 'block', 'number_of_rooms', 'type_of_room', 'price', 'land_right', 'year', 'land_area', 'building_area', 'station', 'walking_distance_station')
+                                                ->wherePlan($request)
+                                                ->whereAddress($request)
+                                                ->whereLowestPrice($request)
+                                                ->whereHighestPrice($request)
+                                                ->whereLandRight($request)
+                                                ->whereLowestLandArea($request)
+                                                ->whereHighestLandArea($request)
+                                                ->whereLowestBuildingArea($request)
+                                                ->whereHighestBuildingArea($request)
+                                                ->whereStation($request)
+                                                ->whereWalkingDistanceStation($request)
+                                                ->whereOld($request)
+                                                ->paginate(15);
+        $request->session()->regenerateToken();
+
+        return view('admin.old_detached_house.result', compact('old_detached_houses', 'request'));
+    }
+    public function showDetail($id) {
+        $old_detached_house = OldDetachedHouse::find($id);
+        return view('admin.old_detached_house.detail', compact('old_detached_house'));
+    }
+    public function edit($id) {
+        $old_detached_house = OldDetachedHouse::find($id);
+        return view('admin.old_detached_house.edit', compact('old_detached_house'));
+    }
+    public function update($id, OldDetachedHouseSignUpRequest $request) {
+        $old_detached_house = OldDetachedHouse::find($id);
+        $old_detached_house->updateOldDetachedHouse($id, $request);
+        $request->session()->regenerateToken();
+        return redirect()->route('admin.oldDetachedHouse.edit', ['id' => $old_detached_house->id])->with('message', '変更内容を登録しました。');
+    }
+}
---
+++ .history/app/Providers/AppServiceProvider_20220822213309.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Collection;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Collection::macro('exportCsv', function($file_name = '', $item_name = [], $filters = [], $encoding = 'UTF-8') {
+
+            if(empty($file_name)) {
+                $file_name = $this->first()->getTable() .'_'. date('Ymd_His') .'.csv';
+            }
+
+            $fluent = \FluentCsv::setEncoding($encoding);
+
+            if(!empty($item_name)) {
+                $fluent->addData($item_name);
+            }
+
+            $this->each(function($item) use($fluent, $filters) {
+
+                $row_data = [];
+
+                if(empty($filters)) {
+
+                    $row_data = array_values($item->toArray());
+
+                } else {
+
+                    foreach($filters as $filter) {
+
+                        if(is_callable($filter)) {
+
+                            $row_data[] = $filter($item);
+
+                        }
+
+                    }
+
+                }
+
+                $fluent->addData($row_data);
+
+            });
+
+            return $fluent->download($file_name);
+
+        });
+    }
+}
