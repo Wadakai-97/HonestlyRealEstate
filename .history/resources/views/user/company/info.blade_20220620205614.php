8e59c9562ea9e9c51c36b67c63a4a9e267ea575f
Wadakai-97 <wadakai1111@gmail.com>
2022-09-05 18:43:53 +0900

    画像バリデーションの修正

---
+++ .history/resources/views/user/company/info.blade_20220620205614.php
@@ -0,0 +1,8 @@
+@extends('layouts.user')
+@section('title', '会社概要')
+@section('body')
+<p>会社概要</p>
+
+<p>住所</p>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.82413677502!2d139.76446941162123!3d35.6813320424092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfbd89f700b%3A0x277c49ba34ed38!2z5p2x5Lqs6aeF!5e0!3m2!1sja!2sjp!4v1654154652908!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+@endsection
